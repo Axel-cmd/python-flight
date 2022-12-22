71c579f24fa436058c20b25f773a15f8f6b031fa
AXELMINART <77530883+Axel-cmd@users.noreply.github.com>
2022-12-22 15:51:59 +0100

    Update flight_map.py

--- flight_map.py
+++ flight_map.py
@@ -1,102 +1,107 @@
 import csv
 from airport import Airport
 from flight import Flight
+from flight_path import FlightPath
+
+def formatCsvString(text: str) -> str:
+    return text.replace('"', "").replace(" ", "")
+
 
 class FlightMap:
-	def __init__(self) -> None:
-		# dictionnaire des aéroports
-		self.__airports = {}
-		#Liste des vols 
-		self.__flights = []
+    def __init__(self) -> None:
+        # dictionnaire des aéroports
+        self.__airports = {}
+        #Liste des vols 
+        self.__flights = []
 
-	def airports(self) -> list[Airport]:
-		return self.__airports.values()
+    def airports(self) -> list[Airport]:
+        return self.__airports.values()
 
-	def flights(self) -> list[Flight]:
-		return self.__flights
+    def flights(self) -> list[Flight]:
+        return self.__flights
 
-	def import_airports(self, csv_file: str) -> None:
+    def import_airports(self, csv_file: str) -> None:
 
-		with open(csv_file, 'r') as file:
-			reader = csv.reader(file)
+        with open(csv_file, 'r') as file:
+            reader = csv.reader(file)
 
-			for row in reader:
+            for row in reader:
 
-				name, code, latitude, longitude = row
+                name, code, latitude, longitude = row
 
-				#formater les données récupéré
-				name, code = formatString(name), formatString(code)
-				latitude = float(formatString(latitude))
-				longitude = float(formatString(longitude))
+                #formater les données récupéré
+                name, code = formatCsvString(name), formatCsvString(code)
+                latitude = float(formatCsvString(latitude))
+                longitude = float(formatCsvString(longitude))
 
-				airport = Airport(name, code, latitude, longitude)
+                airport = Airport(name, code, latitude, longitude)
 
-				self.__airports[code] = airport
+                self.__airports[code] = airport
 
-	def import_flights(self, csv_file: str) -> None:
-		with open(csv_file, 'r') as file:
-			reader = csv.reader(file)
+    def import_flights(self, csv_file: str) -> None:
+        with open(csv_file, 'r') as file:
+            reader = csv.reader(file)
 
-			for row in reader:
-				origin, destination, duration = row
+            for row in reader:
+                origin, destination, duration = row
 
-				origin, destination = formatString(origin), formatString(destination)
+                origin, destination = formatCsvString(origin), formatCsvString(destination)
 
-				try :
-					duration = float(duration)
-				except ValueError:
-					print('Erreur dans le CSV')
-					continue
+                try :
+                    duration = float(duration)
+                except ValueError:
+                    print('Erreur dans le CSV')
+                    continue
 
+                flight = Flight(origin, destination, duration)
+                self.__flights.append(flight)
 
-
-				flight = Flight(origin, destination, duration)
-
-				self.__flights.append(flight)
-
-	def airport_find(self, airport_code: str) -> Airport:
-		try :
-			airport = self.__airports[airport_code]
-			return airport
-		except KeyError:
-			print(f"L'aéroport {airport_code} n'a pas été trouvé !")
-			return None
-	
-	def flight_exist(self, src_airport_code: str, dst_airport_code: str) -> bool :
-		for flight in self.__flights:
-			if flight.src_code == src_airport_code \
-				and flight.dst_code == dst_airport_code:
-				return True
-			else : 
-				return False	
-			
-	def flights_where(self, airport_code: str) -> list[Flight]:
-		flights = []
-		for flight in self.__flights:
-			if flight.src_code == airport_code or flight.dst_code == airport_code:
-
-				flights.append(flight)
-		return flights
-
-	def airports_from(self, airport_code: str) -> list[Airport]:
-		destinations = []
-
-		for flight in self.__flights:
-
-			if flight.src_code == airport_code:
-				airport = self.__airports[flight.dst_code]
-			elif flight.dst_code == airport_code:
-				airport =  self.__airports[flight.src_code]
-			else : 
-				continue
-
-			if airport not in destinations:
-				print(airport.code)
-				destinations.append(airport)
+    def airport_find(self, airport_code: str) -> Airport:
+        try :
+            airport = self.__airports[airport_code]
+            return airport
+        except KeyError:
+            print(f"L'aéroport {airport_code} n'a pas été trouvé !")
+            return None
+    
+    def flight_exist(self, src_airport_code: str, dst_airport_code: str) -> bool :
+        for flight in self.__flights:
+            if flight.src_code == src_airport_code \
+                and flight.dst_code == dst_airport_code:
+                return True
+            else : 
+                return False	
+            
+    def flights_where(self, airport_code: str) -> list[Flight]:
+        flights = []
+        for flight in self.__flights:
+            if flight.src_code == airport_code or flight.dst_code == airport_code:
+                flights.append(flight)
+                
+        return flights
 
 
-		return destinations
+    def airports_from(self, airport_code: str) -> list[Airport]:
+      destinations = []
+
+      for flight in self.__flights:
+
+        if flight.src_code == airport_code:
+            airport = self.__airports[flight.dst_code]
+        elif flight.dst_code == airport_code:
+            airport =  self.__airports[flight.src_code]
+        else : 
+            continue
+
+        if airport not in destinations:
+            print(airport.code)
+            destinations.append(airport)
+
+      return destinations
+
+    def paths(src_airport_code: str, dst_airport_code: str) -> list[FlightPath]:
+        pass
+        
+    
 
 
-def formatString(text: str) -> str:
-	return text.replace('"', "").replace(" ", "")
