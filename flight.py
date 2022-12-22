574eee8ecff2f0d0e19265b6e469d64e964fc248
AXELMINART <77530883+Axel-cmd@users.noreply.github.com>
2022-12-22 14:45:49 +0100

    Implémentation des méthodes de flightPath

--- flight.py
+++ flight.py
@@ -1,5 +1,7 @@
 import csv
 from airport import Airport
+from math import fsum
+
 
 def formatString(text: str) -> str:
 	return text.replace('"', "").replace(" ", "")
@@ -10,6 +12,9 @@ class Flight:
 		self.src_code = src_code
 		self.dst_code = dst_code
 		self.duration = duration
+
+	def __str__(self) -> str:
+		return f"source : {self.src_code}, dest : {self.dst_code}, duration: {self.duration}"
 
 
 class FlightMap:
@@ -109,11 +114,31 @@ class FlightMap:
 
 class FlightPath: 
 	def __init__(self, src_airport: Airport) -> None:
-		self.src_airport = src_airport
-		self.path = []
+		self.__airports = []
+		self.__airports.append(src_airport)
+		self.__path: list[Flight] = []
+		
 
 	def add(self, dst_airport : Airport, via_flight : Flight) -> None :
-		pass
+		"""Ajouter un vol au chemin"""
+		if via_flight.src_code == self.__airports[-1].code :
+			self.__path.append(via_flight)
+			self.__airports.append(dst_airport)
+		else : 
+			raise FlightPathBroken("Problème")
+
+	def flights(self) -> list[Flight]:
+		return self.__path
+
+	def airports(self) -> list[Airport]:
+		return self.__airports
+
+	def steps(self) -> float:
+		return len(self.__path)
+
+	def duration(self) -> float:
+		return fsum([flight.duration for flight in self.__path])
+
 
 
 class FlightPathBroken(Exception):
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flight import FlightMap
-
+from flight import FlightMap, FlightPath
 
 def main(): 
 
@@ -11,14 +10,33 @@ def main():
 	# print(flight_map.airport_find("SEA"))
 
 
-	print(flight_map.flights_where("SEA"))
 
-	print(flight_map.flight_exist("AKL", "CPT"))
-	print(flight_map.flight_exist("AKL", "CDG"))
+	# print(flight_map.flight_exist("AKL", "CPT"))
+	# print(flight_map.flight_exist("AKL", "CDG"))
 
-	print(flight_map.airports_from("CDG"))
-	print(flight_map.airports_from("CD"))
+	# print(flight_map.airports_from("CDG"))
+	# print(flight_map.airports_from("CD"))
 
+	airport = flight_map.airport_find("CDG")
+	airport2 = flight_map.airport_find("AMS")
+
+	flights = flight_map.flights_where("CDG")
+	# print(flights)
+
+	for flight in flights:
+		print(flight)
+
+	flight_path = FlightPath(airport)
+	flight_path.add(airport2, flights[2])
+
+
+
+
+
+	print(flight_path.flights())
+	print(flight_path.airports())
+	print(flight_path.steps())
+	print(flight_path.duration())
 
 
 if __name__ == "__main__":
